4b59ebb69f1b0efb032a700a26f14657a3b9174a
ezralee0010 <122120949+ezralee0010@users.noreply.github.com>
2026-02-25 18:24:23 -0600

    updating

--- javascript/modal.js
+++ javascript/modal.js
@@ -2,7 +2,7 @@ const data = {
   journal: {
     title: 'Junk Journal',
     pages: [
-      'Deep in the lower drawer of my cabinet is a collection of random scraps. Some yellowing with age. And some wrinkling under the pressure of those above. I like to think that they’re happily rotting in my drawer. But that is likely not the case. These scraps are arbitrary collections of items I have collected while traveling. Maps, brochures, movie tickets, museum admissions passes. Wherever I go, I just have to take something physical. Something that would remind me that I was physically present there. <br><br> But it\’s always haunted me that these scraps are just sitting there as newer, fresher pieces of paper pile above them. They all deserve to be spotlighted. Well, that\’s my belief. I recently bought a journal to start journaling. The technical term, I suppose, is junk journaling. Junk journaling consists of accumulating random materials to be pasted onto a journal. My precious scraps will be cut and sliced to shapes that will make them unrecognizable. But my hope is that they will find their new home better. <br><br> I started this journal with my recent study abroad trip to Paris alongside my weekend trips to Prague, Rome, Mainz, and other French cities. The following pages consist of select pictures of my junk journal.',
+      'Deep in the lower drawer of my cabinet is a collection of random scraps. Some yellowing with age. And some wrinkling under the pressure of those above. I like to think that they’re happily rotting in my drawer. But that is likely not the case. These scraps are arbitrary collections of items I have collected while traveling. Maps, brochures, movie tickets, museum admissions passes. Wherever I go, I just have to take something physical. Something that would remind me that I was physically present there. <br><br> But it\’s always haunted me that these scraps are just sitting there as newer, fresher pieces of paper pile above them. They all deserve to be spotlighted. Well, that\’s my belief. I recently bought a journal to start journaling. The technical term, I suppose, is junk journaling. Junk journaling consists of accumulating random materials to be pasted onto a journal. My precious scraps will be cut and sliced to shapes that will make them unrecognizable. But my hope is that they will find their new home better. <br><br> I started this journal with my recent study abroad trip to Paris alongside my weekend trips to Prague, Rome, Mainz, and some French cities. The following pages consist of select pictures of my junk journal.',
       '<img src="images/journal-images/IMG_9732.jpeg">',
       '<img src="images/journal-images/IMG_9735.jpeg">'
     ]
@@ -10,8 +10,9 @@ const data = {
   game: {
     title: 'Some internet stuff',
     pages: [
-      '<iframe src="https://ezralee0010.github.io/css-art/" width="300" height="300" frameborder="0" allowfullscreen=""> </iframe>', 
-      '<iframe frameborder="0" src="https://itch.io/embed-upload/7878672?color=333333" allowfullscreen="" width="640" height="370"><a href="https://ezralee.itch.io/dating-sim-new-student-at-stuy">Play Dating Simulator</a></iframe>'
+      'Unfortunately, there are various pieces of media that are posted online. Some I am proud of, others I would rather not discuss. I’ve accumulated some pieces I think you as my audience might enjoy. Hopefully… No promises.',
+      'CSS Art from my MADD 23631 class <br><br> <iframe src="https://ezralee0010.github.io/css-art/" width="300" height="300" frameborder="0" allowfullscreen=""> </iframe>', 
+      'This random visual novel I made based off of a story I wrote for my high school newspaper <br><br> <iframe frameborder="0" src="https://itch.io/embed-upload/7878672?color=333333" allowfullscreen="" width="640" height="370"><a href="https://ezralee.itch.io/dating-sim-new-student-at-stuy">Play Dating Simulator</a></iframe>'
     ]
   },
   cd: {
@@ -57,9 +58,9 @@ const data = {
     ]
   },
   lights: {
-    title: 'Lights',
+    title: 'Lights On',
     pages: [
-      'hi'
+      'You have entered my space. Who are you? Why are you here? How did you enter? I guess I’ll never know.'
     ]
   },
   board: {
