6db467f7bdade02024eec7ef58488f1a931e793d
ezralee0010 <122120949+ezralee0010@users.noreply.github.com>
2026-02-26 02:46:16 -0600

    movies added

--- javascript/data.js
+++ javascript/data.js
@@ -3,12 +3,19 @@ const data = {
     title: 'Junk Journal',
     pages: [
       'Deep in the lower drawer of my cabinet is a collection of random scraps. Some yellowing with age. And some wrinkling under the pressure of those above. I like to think that they’re happily rotting in my drawer. But that is likely not the case. These scraps are arbitrary collections of items I have collected while traveling. Maps, brochures, movie tickets, museum admissions passes. Wherever I go, I just have to take something physical. Something that would remind me that I was physically present there. <br><br> But it\’s always haunted me that these scraps are just sitting there as newer, fresher pieces of paper pile above them. They all deserve to be spotlighted. Well, that\’s my belief. I recently bought a journal to start journaling. The technical term, I suppose, is junk journaling. Junk journaling consists of accumulating random materials to be pasted onto a journal. My precious scraps will be cut and sliced to shapes that will make them unrecognizable. But my hope is that they will find their new home better. <br><br> I started this journal with my recent study abroad trip to Paris alongside my weekend trips to Prague, Rome, Mainz, and some French cities. The following pages consist of select pictures of my junk journal.',
+      
       'Some museums in Paris <br><br> <img src="images/journal-images/IMG_9732.jpeg">',
+      
       'Versailles <br><br> <img src="images/journal-images/IMG_9735.jpeg">',
+      
       'Paris <br><br> <img src="images/journal-images/IMG_9734.jpeg">',
+      
       'Mainz <br><br> <img src="images/journal-images/IMG_9737.jpeg">',
+      
       'Rome + Vatican City <br><br> <img src="images/journal-images/IMG_9740.jpeg">',
+      
       'French Cities <br><br> <img src="images/journal-images/IMG_9736.jpeg">',
+      
       'Prague <br><br> <img src="images/journal-images/IMG_9741.jpeg">'
     ]
   },
@@ -16,20 +23,28 @@ const data = {
     title: 'Some Internet Stuff',
     pages: [
       'Unfortunately, there are various pieces of media of mine that are posted online. Some I am proud of, others I would rather not discuss. This is a collection of some of those pieces. I hope no one sees these…',
+      
       'CSS Art from my MADD 23631 class <br><br> <iframe src="https://ezralee0010.github.io/css-art/" width="100%" height="500px" frameborder="0" allowfullscreen=""> </iframe>', 
+      
       'This random visual novel I made based off of a story I wrote for my high school newspaper <br><br> <iframe frameborder="0" src="https://itch.io/embed-upload/7878672?color=333333" allowfullscreen="" width="100%" height="300"><a href="https://ezralee.itch.io/dating-sim-new-student-at-stuy">Play Dating Simulator</a></iframe>',
+      
       'Some articles from back in the days <br><br> <iframe src="https://stuyspec.com/staff/ezra-lee" width="100%" height="500" frameborder="0" allowfullscreen=""> </iframe>'
     ]
   },
   cd: {
-    title: 'Music',
+    title: 'Music', 
     pages: [
       'I have horrible music taste. Yes, I repeat Spotify\’s Today\’s Top Hits playlist on the daily. Yes, I only know the mainstream stuff. But here are some featured songs and artists anyway.',
-      'Laufey <br><br> <iframe width="100%" height="300" src="https://www.youtube.com/embed/Yq6qkq_TWAM?autoplay=1&loop=1" allow="autoplay; encrypted-media" frameborder="0"></iframe>',
-      'PinkPantheress + Zara Larsson <br><br> <iframe width="100%" height="300" src="https://www.youtube.com/embed/lIxQe1R5hs0?autoplay=1&loop=1" allow="autoplay; encrypted-media" frameborder="0"></iframe>',
-      'beabadoobee <br><br> <iframe width="100%" height="300" src="https://www.youtube.com/embed/HwQcYL3Cpm0?autoplay=1&loop=1" allow="autoplay; encrypted-media" frameborder="0"></iframe>',
-      'wave to earth <br><br> <iframe width="100%" height="300" src="https://www.youtube.com/embed/Q49pnA4jsp8?autoplay=1&loop=1" allow="autoplay; encrypted-media" frameborder="0"></iframe>',
-      'Ravyn Lenae <br><br> <iframe width="100%" height="300" src="https://www.youtube.com/embed/xD0Q8ZMfZck?autoplay=1&loop=1" allow="autoplay; encrypted-media" frameborder="0"></iframe>'
+      
+      'Laufey <br><br> <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/08PdFBcXzpkn1cWNgmKqhn?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+      
+      'PinkPantheress + Zara Larsson <br><br> <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/1DwscornXpj8fmOmYVlqZt?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+      
+      'beabadoobee <br><br> <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/0OpC8hRrbsh24taKOsQBEX?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+      
+      'wave to earth <br><br> <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/5mtTAScDytxMMqZj14NmlN?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+      
+      'Ravyn Lenae <br><br> <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/2od6VwQ93pD6vfYss3oync?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
     ]
   },
   rug: {
@@ -47,7 +62,7 @@ const data = {
   movie: {
     title: 'TV',
     pages: [
-      'So, I don\'t actually have a projector or screen in my room. But that\'s okay! I can design my online room to be whatever I want!'
+      'I\’m not really a movie person, which is ironic because the clip shown on this digital projector is from the movie Everything Everywhere All at Once. During the last day of high school, I watched this film with some friends. We skipped our classes and pulled it up on some illegal site on a friend’s phone. Funnily enough, the teacher I skipped class for actually passed by us while the movie was playing. Very awkward. But it was worth it. <br><br> Sorry, got a bit off track. <br><br> On the flip side, I love TV shows. Especially long ones. I love how I can just sit down and binge watch a series for a couple hours, leave it alone for some time, and then come back as if I never stopped. The story and world seem to never end. Until it does. And then I get sad. So I actually tend to avoid watching the series finale. Just so I don\’t have to say goodbye. It\’s a horrible habit of mine. <br><br> I\’ll show a few I’ve watched recently.'
     ]
   },
   bed: {
