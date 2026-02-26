3f72b06e47dbea1f3dd7e79c8b67cf89a431f681
ezralee0010 <122120949+ezralee0010@users.noreply.github.com>
2026-02-26 00:31:33 -0600

    video added

---
+++ javascript/data.js
@@ -0,0 +1,83 @@
+const data = {
+  journal: {
+    title: 'Junk Journal',
+    pages: [
+      'Deep in the lower drawer of my cabinet is a collection of random scraps. Some yellowing with age. And some wrinkling under the pressure of those above. I like to think that they’re happily rotting in my drawer. But that is likely not the case. These scraps are arbitrary collections of items I have collected while traveling. Maps, brochures, movie tickets, museum admissions passes. Wherever I go, I just have to take something physical. Something that would remind me that I was physically present there. <br><br> But it\’s always haunted me that these scraps are just sitting there as newer, fresher pieces of paper pile above them. They all deserve to be spotlighted. Well, that\’s my belief. I recently bought a journal to start journaling. The technical term, I suppose, is junk journaling. Junk journaling consists of accumulating random materials to be pasted onto a journal. My precious scraps will be cut and sliced to shapes that will make them unrecognizable. But my hope is that they will find their new home better. <br><br> I started this journal with my recent study abroad trip to Paris alongside my weekend trips to Prague, Rome, Mainz, and some French cities. The following pages consist of select pictures of my junk journal.',
+      'Some museums in Paris <br><br> <img src="images/journal-images/IMG_9732.jpeg">',
+      'Versailles <br><br> <img src="images/journal-images/IMG_9735.jpeg">',
+      'Paris <br><br> <img src="images/journal-images/IMG_9734.jpeg">',
+      'Mainz <br><br> <img src="images/journal-images/IMG_9737.jpeg">',
+      'Rome + Vatican City <br><br> <img src="images/journal-images/IMG_9740.jpeg">',
+      'French Cities <br><br> <img src="images/journal-images/IMG_9736.jpeg">',
+      'Prague <br><br> <img src="images/journal-images/IMG_9741.jpeg">'
+    ]
+  },
+  game: {
+    title: 'Some Internet Stuff',
+    pages: [
+      'Unfortunately, there are various pieces of media of mine that are posted online. Some I am proud of, others I would rather not discuss. This is a collection of some of those pieces. I hope no one sees these…',
+      'CSS Art from my MADD 23631 class <br><br> <iframe src="https://ezralee0010.github.io/css-art/" width="100%" height="500px" frameborder="0" allowfullscreen=""> </iframe>', 
+      'This random visual novel I made based off of a story I wrote for my high school newspaper <br><br> <iframe frameborder="0" src="https://itch.io/embed-upload/7878672?color=333333" allowfullscreen="" width="100%" height="300"><a href="https://ezralee.itch.io/dating-sim-new-student-at-stuy">Play Dating Simulator</a></iframe>',
+      'Some articles from back in the days <br><br> <iframe src="https://stuyspec.com/staff/ezra-lee" width="100%" height="500" frameborder="0" allowfullscreen=""> </iframe>'
+    ]
+  },
+  cd: {
+    title: 'Music',
+    pages: [
+      'I have horrible music taste. Yes, I repeat Spotify\’s Today\’s Top Hits playlist on the daily. Yes, I only know the mainstream stuff. But here are some featured songs and artists anyway.',
+      'Laufey <br><br> <iframe width="100%" height="300" src="https://www.youtube.com/embed/Yq6qkq_TWAM" frameborder="0"></iframe>',
+      'PinkPantheress + Zara Larsson <br><br> <iframe width="100%" height="300" src="https://www.youtube.com/embed/lIxQe1R5hs0" frameborder="0"></iframe>',
+      'beabadoobee <br><br> <iframe width="100%" height="300" src="https://www.youtube.com/embed/HwQcYL3Cpm0" frameborder="0"></iframe>',
+      'wave to earth <br><br> <iframe width="100%" height="300" src="https://www.youtube.com/embed/Q49pnA4jsp8" frameborder="0"></iframe>',
+      'Ravyn Lenae <br><br> <iframe width="100%" height="300" src="https://www.youtube.com/embed/xD0Q8ZMfZck" frameborder="0"></iframe>'
+    ]
+  },
+  rug: {
+    title: 'Rug',
+    pages: [
+      'My parents put a temporary rug in my room, only until we\’d get a new one. That replacement never came. It\’s this Mickey Mouse rug with a wizard hat, and somehow it\’s been with me for more than half of my life. At this point, it probably deserves retirement. '
+    ]
+  },
+  table: {
+    title: 'The Table',
+    pages: [
+      'hi'
+    ]
+  }, 
+  movie: {
+    title: 'TV',
+    pages: [
+      'So, I don\'t actually have a projector or screen in my room. But that\'s okay! I can design my online room to be whatever I want!'
+    ]
+  },
+  bed: {
+    title: 'The Bed',
+    pages: [
+      'This is my bed. GET OUT!'
+    ]
+  },
+  wall: {
+    title: 'Walls',
+    pages: [
+      'hi'
+    ]
+  },
+  window: {
+    title: 'Window',
+    pages: [
+      'hi'
+    ]
+  },
+  lights: {
+    title: 'Lights On',
+    pages: [
+      'You have entered my space. Who are you? Why are you here? How did you enter? I guess I’ll never know.'
+    ]
+  },
+  board: {
+    title: 'Vision Board',
+    pages: [
+      'I bought this cork board at the Dollar Tree to be my mood/vision board. 5 years ago. It\’s still completely empty. At this point, it feels like it might never be finished.'
+    ]
+  }
+}
