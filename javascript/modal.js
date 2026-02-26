b8f70b5e355d6422d17b187e4bf71bbd0b46d5b9
ezralee0010 <122120949+ezralee0010@users.noreply.github.com>
2026-02-25 18:29:56 -0600

    journal entries

--- javascript/modal.js
+++ javascript/modal.js
@@ -3,8 +3,10 @@ const data = {
     title: 'Junk Journal',
     pages: [
       'Deep in the lower drawer of my cabinet is a collection of random scraps. Some yellowing with age. And some wrinkling under the pressure of those above. I like to think that they’re happily rotting in my drawer. But that is likely not the case. These scraps are arbitrary collections of items I have collected while traveling. Maps, brochures, movie tickets, museum admissions passes. Wherever I go, I just have to take something physical. Something that would remind me that I was physically present there. <br><br> But it\’s always haunted me that these scraps are just sitting there as newer, fresher pieces of paper pile above them. They all deserve to be spotlighted. Well, that\’s my belief. I recently bought a journal to start journaling. The technical term, I suppose, is junk journaling. Junk journaling consists of accumulating random materials to be pasted onto a journal. My precious scraps will be cut and sliced to shapes that will make them unrecognizable. But my hope is that they will find their new home better. <br><br> I started this journal with my recent study abroad trip to Paris alongside my weekend trips to Prague, Rome, Mainz, and some French cities. The following pages consist of select pictures of my junk journal.',
-      '<img src="images/journal-images/IMG_9732.jpeg">',
-      '<img src="images/journal-images/IMG_9735.jpeg">'
+      'Some museums in Paris <br><br> <img src="images/journal-images/IMG_9732.jpeg">',
+      'Versailles <br><br> <img src="images/journal-images/IMG_9735.jpeg">',
+      'Paris <br><br> <img src="images/journal-images/IMG_9734.jpeg">',
+      'Mainz <br><br> <img src="images/journal-images/IMG_9737.jpeg">'
     ]
   },
   game: {
